34d7872a53df4bcd7132d3f33f685d3d28fc4510
Tu Vu <vuanhtu52@gmail.com>
2023-10-16 13:34:38 +0700

    Set up eslint and prettier

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./style.css";
+
+const name = "James";
+
+const person = { first: name };
+
+console.log(person);
+
+const sayHelloLinting = (fName) => {
+    console.log(`Hello linting, ${fName}`);
+};
